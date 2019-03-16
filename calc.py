b85f24c66dd67d9babef23d8eb3db408c58f1bf5
pennyz <xzheng2@babson.edu>
2019-03-16 15:10:35 -0400

    Penny Zheng's

---
+++ calc.py
@@ -0,0 +1,88 @@
+# print hello world
+# pombxifibrtu 7y  \
+# print "hello workds" \
+#print("hwy jude", "do not make bad")
+#print('1+2+3=', 1+2+3)
+# print(name)
+#
+# print('hello, ', name)
+#
+# first_name ='lennon'
+# last_name = 'john'
+# name = last_name + " " + first_name
+# print('hello, {}! You are {} years old.'.format(name, 20))
+# print('Pi equals {:.2f}'. format(3.1415926))
+
+# Excercise 2.1
+NUMBER_OF_SECONDS_IN_ONE_MINUTE = 60
+seconds = 42 * 60 + 42
+print("There are {}seconds...".format (seconds))
+
+# Excercise 2.2
+# NUMBER_OF_MILES_IN_ONE_KILOMETERS = 1.61
+# Kilometers = 10/NUMBER_OF_MILES_IN_ONE_KILOMETERS
+# print("There are {} miles...".format())
+
+# Excercise 1.1 in chapter 1
+
+hour =13
+min = 2
+print("current time is {:02}:{:02}.".format(hour, min))
+print (1+2)
+
+#
+# import math
+# r=5
+# volume = (4/3)*math.pi*math.pow(r,3)
+# print('The volume is of a sphere of {} is {:.3f}.'.format (r,volume))
+#
+# import math
+# r=input("Please enter a number:")
+# r=int(r)
+# volume = (4/3)*math.pi*math.pow(r,3)
+# print('The volume is of a sphere of {} is {:.3f}.'.format(r,volume))
+#
+# import math
+# r=input("Please enter a number:")
+# r=float(r)
+# volume = (4/3)*math.pi*math.pow(r,3)
+# print('The volume is of a sphere of {} is {:.3f}.'.format(r,volume))
+1.3
+# import datetime
+# print(datetime.datetime.now())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter2.py
@@ -0,0 +1,86 @@
+# print(3.14e-2)
+# length_of_number = len(str(2**1000000))
+# print(length_of_number)
+#
+# import math
+# print(math.pi)
+# print(math.sqrt(25))
+#
+# import random
+# print(int(random.random()*100))  # a random integer
+#
+# print(random.randint(1,6)) # a random integer between 1 and 6
+# print(random.choice(["a","b","c"])) # a random name in the list
+
+# import random
+# lower = int(input("Please enter a number as the lower bound:"))
+# upper = int(input("another one as upper bound:"))
+# print(random.randint(lower,100))
+#
+#
+# ## strings
+# # I'm "OK".
+# print("I'm ")
+# print("I'm saying \"OK\".")
+# print('I\'m learning\n\nPython.')
+
+# ## Boolean
+# print(3>2 and 3>5)
+# print(3>2 or 3>5 or False or 100 <100)
+# True and true
+#  a==1 ==: assignment
+# # age = int(input("hOW OLD ARE YOU"))
+# ag`e = int(input("Please enter your age:"))
+# is_in_US = True
+# if age>=21: # boolean
+#     print('Yes, you can.')
+# else:
+#     print('Sorry, you cannot.')
+#
+# (1!=1)-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
